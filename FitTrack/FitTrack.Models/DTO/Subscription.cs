8705fba4624fe2674920a4ef5ff05f0912a22580
Stanislava Germanova <114156419+StanislavaGermanovaa@users.noreply.github.com>
2025-01-18 18:00:58 +0200

    inti models

---
+++ FitTrack/FitTrack.Models/DTO/Subscription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack.Models.DTO
+{
+    public class Subscription
+    {
+        public string Id { get; set; } 
+        public string UserId { get; set; } 
+        public string Type { get; set; } 
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
